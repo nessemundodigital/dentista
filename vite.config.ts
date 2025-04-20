7582d7cdd1b9386c93d2edf26f224171f21a8633
nessemundodigital <nessemundodigital@gmail.com>
2025-04-20 13:06:44 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "context": path.resolve(__dirname, "./src/context"),
+"components": path.resolve(__dirname, "./src/components"),
+
+    },
+  },
+}));
